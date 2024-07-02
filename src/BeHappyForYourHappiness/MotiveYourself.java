11c14d0eee3246408e670a50fd52da834ecbf65a
srinani02 <srilakshminani1234@gmail.com>
2024-07-02 08:58:34 +0530

    stay loyal but not stupid

---
+++ src/BeHappyForYourHappiness/MotiveYourself.java
@@ -0,0 +1,10 @@
+package BeHappyForYourHappiness;
+
+public class MotiveYourself {
+
+	public static void main(String[] args)
+	{
+       System.out.println("every mindset is not same because of every all fingers are not equal");
+	}
+
+}
